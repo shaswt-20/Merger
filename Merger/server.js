94fabd04fddbfaae69cd34d37b04b86aba0795f3
shaswt-20 <127007759+shaswt-20@users.noreply.github.com>
2023-04-11 14:29:40 +0530

    Add files via upload

---
+++ Merger/server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const path = require("path")
+const app = express()
+const PDFMerger = require('pdf-merger-js');
+var mer2 = new PDFMerger();
+
+const multer  = require('multer')
+const {mergePdfs} = require('./merge')
+const upload = multer({ dest: 'uploads/' })
+
+app.use('/static', express.static('public'))
+app.use(express.static(path.join(__dirname,'public')))
+const port = 3000 || process.env.PORT
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, "templates/index.html"));
+});
+
+app.post('/merge', upload.array('pdfs', 2), async(req, res, next) =>{
+   console.log(req.files)
+   let d = await mergePdfs(path.join(__dirname, req.files[0].path), path.join(__dirname, req.files[1].path))
+   res.redirect(`http://localhost:3000/static/${d}.pdf`)
+})
+
+
+app.listen(port, () => {
+  // console.log(`Example app listening on port http://localhost:${port}`)
+}) 
